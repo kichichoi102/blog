e5e0aa7c9a071308122bf548b964fc84e6b1cf64
kichi.choi <kichi.choi@withyouwithme.com>
2023-01-06 13:26:00 -0500

    changed var types

--- src/repositories/repository-factory.js
+++ src/repositories/repository-factory.js
@@ -1,50 +1,51 @@
-import { BaseRepository } from './base-repository'
+import { BaseRepository } from './base-repository';
 
 const repositories = {
-    posts: {
-        endpoint: 'posts'
-    },
-    comments: {
-        endpoint: 'comments'
-    },
-    albums: {
-        endpoint: 'albums'
-    },
-    photos: {
-        endpoint: 'photos'
-    },
-    todos: {
-        endpoint: 'todos'
-    },
-    users: {
-        endpoint: 'users'
-    },
-}
+  posts: {
+    endpoint: 'posts',
+  },
+  comments: {
+    endpoint: 'comments',
+  },
+  albums: {
+    endpoint: 'albums',
+  },
+  photos: {
+    endpoint: 'photos',
+  },
+  todos: {
+    endpoint: 'todos',
+  },
+  users: {
+    endpoint: 'users',
+  },
+};
 
-// memory of instances so u wouldnt have to create 
+Object.freeze(repositories);
+
+// memory of instances so u wouldnt have to create
 // a new repo everytime its called, rather call from instances
-const instances = {}
+let instances = {};
 
 /**
  * Create factory given endpoint name, getter
  * @param {*} typeName aka endpoint name
  * @returns {Object} repo object
  */
-function createFactory (typeName) {
-    if (instances[typeName] == null || instances[typeName] == undefined) {
-        if (!(typeName in repositories)) {
-            console.log('Endpoint doesnt exist')
-            return null
-        }
-        const endpoint = repositories[typeName].endpoint
-        const repo = new BaseRepository(endpoint)
-
-        instances[typeName] = repo
+function createFactory(typeName) {
+  if (instances[typeName] == null || instances[typeName] == undefined) {
+    if (!(typeName in repositories)) {
+      console.log('Endpoint doesnt exist');
+      return null;
     }
-    return instances[typeName]
+    const endpoint = repositories[typeName].endpoint;
+    const repo = new BaseRepository(endpoint);
+
+    instances[typeName] = repo;
+  }
+  return instances[typeName];
 }
 
 export default {
-    get: name => createFactory(name)
-}
-
+  get: (name) => createFactory(name),
+};
