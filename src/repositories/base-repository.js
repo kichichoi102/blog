3a618b64402d6c41d44bb4301712617e83b28d9c
kichi.choi <kichi.choi@withyouwithme.com>
2023-01-06 12:52:22 -0500

    finalized base-repository

--- src/repositories/base-repository.js
+++ src/repositories/base-repository.js
@@ -4,15 +4,14 @@ import { createClient } from './client.js';
  * Creates Base repo
  * @param {string} controllerPath url
  * @param {string} endpoint endpoint
- * @return {*} client
  */
 class BaseRepository {
   /**
-     * Base repo constructor
-     * @param {string} controllerPath url
-     * @param {string} endpoint endpoint
-     */
-  constructor (controllerPath, endpoint, primaryKeyField) {
+   * Base repo constructor
+   * @param {string} controllerPath url
+   * @param {string} endpoint endpoint
+   */
+  constructor(controllerPath, endpoint, primaryKeyField) {
     this.controllerPath = controllerPath;
     this.endpoint = endpoint;
     this.client = null;
@@ -23,9 +22,9 @@ class BaseRepository {
    * client lazy loading, aka create client if it doesnt exist,
    * else use the existing one
    */
-  _loadClient () {
+  _loadClient() {
     if (this.client == null) {
-      this.client = createClient()
+      this.client = createClient();
     }
   }
 
@@ -33,17 +32,16 @@ class BaseRepository {
    * GET ALL Req call for all entity records for given endpoint
    * @returns {Array.<Object>} An array of all entities of given endpoint
    */
-  async getAsync () {
-
-    this._loadClient()
+  async getAsync() {
+    this._loadClient();
 
     try {
-        const urlString = this.controllerPath + '/' + this.endpoint
-        const res = await this.client.get(urlString)
-        return res.data
+      const urlString = this.controllerPath + '/' + this.endpoint;
+      const res = await this.client.get(urlString);
+      return res.data;
     } catch (error) {
-        console.log(error)
-        return [{}]
+      console.log(error);
+      return [{}];
     }
   }
 
@@ -52,18 +50,68 @@ class BaseRepository {
    * @param {object} id of entity to get
    * @returns {Object} Entity found
    */
-  async getPostAsync (id) {
-
-    this._loadClient()
+  async getPostAsync(id) {
+    this._loadClient();
 
     try {
-        const urlString = this.controllerPath + '/' + this.endpoint + '/' + id
-        const res = this.client.get(urlString)
-        return res.data
+      const urlString = this.controllerPath + '/' + this.endpoint + '/' + id;
+      const res = this.client.get(urlString);
+      return res.data;
     } catch (error) {
-        console.log(error)
-        return {}
+      console.log(error);
+      return {};
     }
   }
 
+  /**
+   * Create an entity
+   * @param {Object} payload - the data of the entity to create
+   */
+  async createAsync(payload) {
+    this._loadClient();
+
+    try {
+      const urlString = this.controllerPath + '/' + this.endpoint;
+      const res = await this.client.post(urlString, payload);
+      return res;
+    } catch (error) {
+      console.log(error);
+      return {};
+    }
+  }
+
+  /**
+   * Updates an entity of the given id
+   * @param {Object} payload
+   * @param {string} id
+   */
+  async updateAsync(payload, id) {
+    this._loadClient();
+
+    try {
+      const urlString = this.controllerPath + '/' + this.endpoint + '/' + id;
+      const res = this.client.patch(urlString, payload);
+      return res;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  /**
+   * Delete entity by id
+   * @param {string} id
+   * @returns
+   */
+  async deleteAsync(id) {
+    this._loadClient();
+
+    try {
+      const urlString = this.controllerPath + '/' + this.endpoint + '/' + id;
+      const res = this.client.delete(urlString);
+      return res;
+    } catch (error) {
+      console.log(error);
+      return {};
+    }
+  }
 }
