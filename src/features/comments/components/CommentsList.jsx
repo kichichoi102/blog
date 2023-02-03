108e945d639ffd1504aecb0d5c569fe8801e5fa9
kichichoi102 <kichichoi102@gmail.com>
2023-02-03 14:05:57 -0500

    fixed routing issues in comments feature

--- src/features/comments/components/CommentsList.jsx
+++ src/features/comments/components/CommentsList.jsx
@@ -1,17 +1,26 @@
 import Accordion from 'react-bootstrap/Accordion';
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import repositoryFactory from '../../../repositories/repository-factory';
 
 export default function CommentsList(postId) {
   const [commentData, setCommentData] = useState([]);
+  const navigate = useNavigate();
+  const commentRepository = repositoryFactory.get('comments');
 
-  async function fetchCommentData() {
-    const commentRepository = repositoryFactory.get('comments');
-    setCommentData(await commentRepository.customRequestAsync('GET', { postId: postId.postId }));
+  async function fetchCommentData(id = null) {
+    if (!id) {
+      navigate('/NotFound');
+    }
+    const comment = await commentRepository.customRequestAsync('GET', { postId: id });
+    if (comment.length == 0) {
+      navigate('/NotFound');
+    }
+    setCommentData(comment);
   }
 
   useEffect(() => {
-    fetchCommentData();
+    fetchCommentData(postId.postId);
   }, []);
 
   return (
